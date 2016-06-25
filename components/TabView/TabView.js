dbda41c8f07b05218c782933c4c21bcc613808b4
Franze Jr <franzejr@gmail.com>
2016-06-25 14:38:37 -0300

    Add Items from API

--- components/TabView/TabView.js
+++ components/TabView/TabView.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component, } from 'react'
 import { PropTypes } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Button from 'react-native-button';
@@ -20,13 +20,21 @@ const styles = StyleSheet.create({
   },
 });
 
+const topicsFromApi = ["Elixir", "Elm", "Elm Remote Meetup", "HTML+CSS", "Modern Development Practices", "Sidekiq"];
+
 const TabView = (props, context) => {
   const drawer = context.drawer;
+  const buttons = topicsFromApi;
+
+  let topics = buttons.map((v,i) => <Button key={i}>{v}</Button>);
+
   return (
     <View style={[styles.container, props.sceneStyle]}>
       <Button onPress={() => { drawer.close(); Actions.mainScreen({type: 'reset'}); }}>Home</Button>
       <Button onPress={() => { drawer.close(); Actions.topicScreen({type: 'reset'}); }}>Topic</Button>
+      { topics }
       <Button onPress={() => { drawer.close(); Actions.settingsScreen({type: 'reset'}); }}>Settings</Button>
+      
     </View>
   );
 };
