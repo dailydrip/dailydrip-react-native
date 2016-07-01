0b1dff15dc65b8fd6cc17c4c96a60abb677dfab8
Josh Adams <josh.rubyist@gmail.com>
2016-07-01 03:01:58 -0600

    Get video playing

--- components/DripScreen/DripScreen.js
+++ components/DripScreen/DripScreen.js
@@ -3,6 +3,7 @@ import { ScrollView, View, WebView, Text, StyleSheet } from 'react-native'
 import { connect } from 'react-redux'
 import Actions from '../../actions'
 import API from '../../api/DailyDripApi'
+import Video from 'react-native-video'
 
 class DripScreen extends Component {
   static propTypes = {}
@@ -17,9 +18,14 @@ class DripScreen extends Component {
 
   render() {
     const drip = this.props.drip || {}
-    console.log(drip)
+    let video = drip.video.url ? (
+        <Video style={styles.video}
+            source={{uri: drip.video.url}}
+        />
+    ) : (<View></View>)
     return (
       <View style={styles.container}>
+        {video}
         <Text style={styles.title}>{drip.title}</Text>
         <WebView style={styles.description} source={ { html: drip.description_html, baseUrl: "https://www.dailydrip.com/" } } />
       </View>
@@ -30,6 +36,9 @@ class DripScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     marginTop: 60,
+    flex: 1
+  },
+  video: {
     flex: 1
   },
   title: {
