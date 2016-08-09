e8b6f0884d4fca364e1a14bb722f5029b2dc5edd
Josh Adams <josh.rubyist@gmail.com>
2016-08-08 21:30:56 -0500

    Extract Drawer component

--- components/App.js
+++ components/App.js
@@ -1,57 +1,12 @@
 import React, { Component } from 'react';
-import { Router, Scene, DefaultRenderer } from 'react-native-router-flux';
-import Drawer from 'react-native-drawer';
+import { Router, Scene } from 'react-native-router-flux';
 
 import MainScreen from './MainScreen/MainScreen';
 import TopicScreen from './TopicScreen/TopicScreen';
 import DripScreen from './DripScreen/DripScreen';
 import SettingsScreen from './SettingsScreen/SettingsScreen';
 import LoginScreen from './LoginScreen/LoginScreen';
-import TabView from './TabView/TabView';
-import variables from './variables';
-
-
-export class DrawerDailyDrip extends Component {
-  static propTypes = {
-    navigationState: React.PropTypes.object,
-  }
-
-  render() {
-    const state = this.props.navigationState;
-    const children = state.children;
-
-    return (
-            <Drawer
-              ref="navigation"
-              open={state.open}
-              type="displace"
-              content={<TabView />}
-              tapToClose
-              openDrawerOffset={0.6}
-              panCloseMask={0.2}
-              negotiatePan
-              styles={{
-                main: {
-                  backgroundColor: variables.dodgerBlue,
-                  shadowColor: '#000000',
-                  shadowOpacity: 0.3,
-                  shadowRadius: 15,
-                },
-                drawer: {
-                  backgroundColor: variables.dodgerBlue,
-                  top: 0,
-                },
-              }}
-              tweenHandler={(ratio) => ({
-                main: { opacity: Math.max(0.54, 1 - ratio) },
-              })}
-            >
-                <DefaultRenderer navigationState={children[0]} onNavigate={this.props.onNavigate} />
-            </Drawer>
-        );
-  }
-}
-
+import Drawer from './Drawer/Drawer';
 
 class App extends Component {
   static propTypes = {}
@@ -59,17 +14,17 @@ class App extends Component {
 
   render() {
     return (
-	    <Router>
-        <Scene key="drawer" component={DrawerDailyDrip} open={false} >
-  	      <Scene key="root">
-  	        <Scene key="mainScreen" component={MainScreen} title="" initial />
+      <Router>
+        <Scene key="drawer" component={Drawer} open={false} >
+          <Scene key="root">
+            <Scene key="mainScreen" component={MainScreen} title="" initial />
             <Scene key="topicScreen" component={TopicScreen} title="" />
             <Scene key="dripScreen" component={DripScreen} title="" />
             <Scene key="settingsScreen" component={SettingsScreen} title="" />
             <Scene key="loginScreen" component={LoginScreen} title="" hideNavBar />
-  	      </Scene>
+          </Scene>
         </Scene>
-	    </Router>
+      </Router>
     );
   }
 }
---
+++ components/Drawer/Drawer.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { DefaultRenderer } from 'react-native-router-flux';
+import NativeDrawer from 'react-native-drawer';
+import TabView from '../TabView/TabView';
+import variables from '../variables';
+
+export class Drawer extends Component {
+  static propTypes = {
+    navigationState: React.PropTypes.object,
+    onNavigate: React.PropTypes.fun,
+  }
+
+  render() {
+    const state = this.props.navigationState;
+    const children = state.children;
+
+    return (
+      <NativeDrawer
+        ref="navigation"
+        open={state.open}
+        type="displace"
+        content={<TabView />}
+        tapToClose
+        openDrawerOffset={0.6}
+        panCloseMask={0.2}
+        negotiatePan
+        styles={{
+          main: {
+            backgroundColor: variables.dodgerBlue,
+            shadowColor: '#000000',
+            shadowOpacity: 0.3,
+            shadowRadius: 15,
+          },
+          drawer: {
+            backgroundColor: variables.dodgerBlue,
+            top: 0,
+          },
+        }}
+        tweenHandler={(ratio) => ({
+          main: { opacity: Math.max(0.54, 1 - ratio) },
+        })}
+      >
+        <DefaultRenderer navigationState={children[0]} onNavigate={this.props.onNavigate} />
+      </NativeDrawer>
+    );
+  }
+}
+
+export default Drawer;
