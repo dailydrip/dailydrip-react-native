fd1ebd2e05d5e04cad5c728cee73adaa64100115
Josh Adams <josh.rubyist@gmail.com>
2016-10-01 05:44:54 -0500

    Make Drip scene pure

--- scenes/Drip.js
+++ scenes/Drip.js
@@ -1,4 +1,4 @@
-import React, { PropTypes, Component } from 'react'
+import React, { PropTypes } from 'react'
 import ImmutablePropTypes from 'react-immutable-proptypes'
 import { connect } from 'react-redux'
 import VideoPlayer from '../components/VideoPlayer/VideoPlayer'
@@ -17,45 +17,42 @@ const styles = StyleSheet.create({
   },
 })
 
-class Drip extends Component {
-  static propTypes = {
-    id: PropTypes.number,
-    drip: ImmutablePropTypes.map,
-    navigate: PropTypes.object,
-  }
+const Drip = ({ drip }) => {
+  let html = ''
 
-  render() {
-    const { drip } = this.props
-    let html = ''
-
-    html += '<html><head>'
-    html += "<link rel='stylesheet' type='text/css' href='drip.css' />"
-    html += "<link rel='stylesheet' type='text/css' href='monokai-sublime.css' />"
-    html += "<script src='highlight.pack.js'></script>"
-    html += "<script src='jquery.min.js'></script>"
-    html += "<script src='fix_redcarpet_syntax_highlighting_indentation.js'></script>"
-    html += '</head><body>'
-    html += drip.get('description_html')
-    html += '<script>$("pre code").prettyPre(); hljs.initHighlightingOnLoad();</script>'
-    html += '</body></html>'
+  html += '<html><head>'
+  html += "<link rel='stylesheet' type='text/css' href='drip.css' />"
+  html += "<link rel='stylesheet' type='text/css' href='monokai-sublime.css' />"
+  html += "<script src='highlight.pack.js'></script>"
+  html += "<script src='jquery.min.js'></script>"
+  html += "<script src='fix_redcarpet_syntax_highlighting_indentation.js'></script>"
+  html += '</head><body>'
+  html += drip.get('description_html')
+  html += '<script>$("pre code").prettyPre(); hljs.initHighlightingOnLoad();</script>'
+  html += '</body></html>'
 
 
-    const video = drip.get('video').get('url') ? (
-      <VideoPlayer source={{ uri: drip.get('video').get('url') }} />
-    ) : (<View />)
-    return (
-      <View style={styles.container}>
-        {video}
-        <WebView
-          style={styles.description}
-          source={{
-            html,
-            baseUrl: 'web',
-          }}
-        />
-      </View>
-    )
-  }
+  const video = drip.get('video').get('url') ? (
+    <VideoPlayer source={{ uri: drip.get('video').get('url') }} />
+  ) : (<View />)
+  return (
+    <View style={styles.container}>
+      {video}
+      <WebView
+        style={styles.description}
+        source={{
+          html,
+          baseUrl: 'web',
+        }}
+      />
+    </View>
+  )
+}
+
+Drip.propTypes = {
+  id: PropTypes.number,
+  drip: ImmutablePropTypes.map,
+  navigate: PropTypes.object,
 }
 
 const mapStateToProps = (state, ownProps) => {
