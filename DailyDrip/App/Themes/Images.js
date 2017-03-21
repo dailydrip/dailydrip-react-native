89dec3c9ddf4747d78a85b9c428e56fdf9603670
Franzé Jr <franzejr@gmail.com>
2017-03-20 23:10:43 -0300

    Add Dailydrip logo

--- DailyDrip/App/Themes/Images.js
+++ DailyDrip/App/Themes/Images.js
@@ -1,9 +1,8 @@
 // leave off @2x/@3x
 const images = {
-  logo: require('../Images/ir.png'),
+  logo: require('../Images/logo_no_name.png'),
   clearLogo: require('../Images/top_logo.png'),
   launch: require('../Images/launch-icon.png'),
-  ready: require('../Images/your-app.png'),
   ignite: require('../Images/ignite_logo.png'),
   igniteClear: require('../Images/ignite-logo-transparent.png'),
   tileBg: require('../Images/tile_bg.png'),
